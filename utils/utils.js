f59b22d896072cd5cdf358c1602d5932c2bb25b4
Harish1 Parihar <Harish1.Parihar@ril.com>
2025-10-28 11:17:03 +0530

    updated backend url

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,5 @@
 // export const apibaseUrl='https://schoolportalbackend-jdar.onrender.com/api/';
-export const apibaseUrl='schoolportalbackend-production.up.railway.app/api/';
+export const apibaseUrl='https://schoolportalbackend-production.up.railway.app/api/';
 // http://localhost:5000/api/students/SCH691962?studentId=STUID20080915  get studenet by id
 // http://localhost:5000/api/students/SCH691962  get all student.
 // http://localhost:5000/api/students/update/SCH691962?studentId=STUID20120325 update student
